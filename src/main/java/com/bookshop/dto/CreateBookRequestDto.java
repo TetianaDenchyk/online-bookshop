ad3ba07b34f9bb8a5636f5d6ca326bd311f6e6b1
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-10-19 18:42:39 +0300

    changed version of checkstyle plugin

--- src/main/java/com/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookshop/dto/CreateBookRequestDto.java
@@ -2,7 +2,6 @@ package com.bookshop.dto;
 
 import java.math.BigDecimal;
 
-
 public record CreateBookRequestDto(String title, String author,
                                   String isbn, BigDecimal price,
                                   String description, String coverImage) {

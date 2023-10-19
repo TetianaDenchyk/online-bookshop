b2dd64a6d1e343d9815ea030c0bf762dea9ea88f
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-10-19 19:17:56 +0300

    used JpaRepository in BookRepository, added liquibase

--- src/main/java/com/bookshop/repository/BookRepository.java
+++ src/main/java/com/bookshop/repository/BookRepository.java
@@ -1,13 +1,10 @@
 package com.bookshop.repository;
 
 import com.bookshop.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository {
-    Book save(Book book);
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
 }

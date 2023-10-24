aec0bcdcd97799b966d9c934525cbcbc0365880a
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-10-24 22:05:57 +0300

    pagination implemented

--- src/main/java/com/bookshop/controller/BookController.java
+++ src/main/java/com/bookshop/controller/BookController.java
@@ -6,6 +6,7 @@ import com.bookshop.service.BookService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,8 +27,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")

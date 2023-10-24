bc21200c242ce2d5322e0fd907124bd53186c7ad
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-10-24 13:06:07 +0300

    ISBN annotation changed for Pattern annotation with regex

--- src/main/java/com/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookshop/dto/CreateBookRequestDto.java
@@ -2,13 +2,16 @@ package com.bookshop.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import org.hibernate.validator.constraints.ISBN;
 
 public record CreateBookRequestDto(@NotBlank String title,
                                    @NotBlank String author,
-                                   @NotBlank @ISBN(type = ISBN.Type.ANY) String isbn,
+                                   @NotBlank @Pattern(
+                                           regexp = "^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\\d-]+$")
+                                   String isbn,
                                    @NotNull @PositiveOrZero BigDecimal price,
                                    String description,
                                    String coverImage) {

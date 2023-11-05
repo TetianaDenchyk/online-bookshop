7d1dde62a28ef769f5985841d6bbc5161af99854
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-05 10:51:49 +0200

    getAll method added in Category Controller

--- src/main/java/com/bookshop/controller/CategoryController.java
+++ src/main/java/com/bookshop/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.bookshop.controller;
 
 import com.bookshop.dto.category.CategoryDto;
 import com.bookshop.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "", description = "")
+@Tag(name = "Category management",
+        description = "Browse and manage categories depending on a role")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/category")
@@ -19,6 +21,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
+    @Operation(summary = "Retrieve all categories", description = "Retrieve categories using pagination")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public List<CategoryDto> getAll(Pageable pageable) {
         return categoryService.findAll(pageable);

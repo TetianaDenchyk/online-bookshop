b6a78d5ce31e458c61175f739c591eefd193c97c
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-16 08:28:02 +0200

    fixed checkstyle error

--- src/main/java/com/bookshop/dto/cartitem/CreateCartItemDto.java
+++ src/main/java/com/bookshop/dto/cartitem/CreateCartItemDto.java
@@ -2,7 +2,6 @@ package com.bookshop.dto.cartitem;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 
 public record CreateCartItemDto(@NotNull Long bookId,
                                 @Positive Integer quantity) {

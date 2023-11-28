0edb5ac32c7e572125a3531c5aa715711cb48aa1
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-28 16:49:27 +0200

    added response status to endpoint in order controller

--- src/main/java/com/bookshop/controller/OrderController.java
+++ src/main/java/com/bookshop/controller/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Order Management",
@@ -40,6 +42,7 @@ public class OrderController {
                         User can create order.
                         After all cart items are deleted.
                         Params: shipping address""")
+    @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('USER')")
     public OrderResponseDto createOrder(Authentication authentication,
                                         @RequestBody @Valid OrderRequestDto requestDto) {

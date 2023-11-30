7eeba825a8936b25553c3f53629284d76d32574b
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-30 18:54:39 +0200

    added info to open api config

--- src/main/java/com/bookshop/config/OpenApiConfig.java
+++ src/main/java/com/bookshop/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.bookshop.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +13,19 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
+        Info info = new Info()
+                .title("Online Bookshop Service API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage and browse books, "
+                        + "shopping cart and orders");
+
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("BearerAuth",
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")))
+                .info(info)
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
     }
 }
